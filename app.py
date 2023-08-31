937b25b28e19b28ea8ec8e0d281c4f50995cec19
YuJia Lim <yujialim0628@gmail.com>
2023-08-31 13:04:48 +0800

    Update the code

--- app.py
+++ app.py
@@ -14,43 +14,50 @@ encoder = joblib.load("ordinal_encoder2.joblib")
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# 1: serious injury, 2: Slight injury, 0: Fatal Injury
+# 1: Major Injury, 2: Minor Injury, 0: Fatal Injury
 
-st.set_page_config(page_title="Accident Severity Prediction App",
-        page_icon="🚧", layout="wide")
+st.set_page_config(page_title=" Prediction of Accident Severity",
+        page_icon="🚗", layout="wide")
 
 #creating option list for dropdown menu
-options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
+options_day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+options_age = ['Under 18', '18-30', '31-50', 'Over 51', 'Unknown']
 
 # number of vehicle involved: range of 1 to 7
 # number of casualties: range of 1 to 8
 # hour of the day: range of 0 to 23
 
-options_types_collision = ['Vehicle with vehicle collision','Collision with roadside objects',
-              'Collision with pedestrians','Rollover','Collision with animals',
-              'Unknown','Collision with roadside-parked vehicles','Fall from vehicles',
-              'Other','With Train']
+options_types_collision = [
+    'Collision of vehicle with vehicle',
+    'Collision with roadside objects',
+    'Collision with pedestrians',
+    'Rollover',
+    'Collision with animals',
+    'Unknown',
+    'Collision with roadside-parked vehicles',
+    'Fall from vehicles',
+    'Other',
+    'With Train']
 
 options_sex = ['Male','Female','Unknown']
 
 options_education_level = ['Junior high school','Elementary school','High school',
               'Unknown','Above high school','Writing & reading','Illiterate']
 
-options_services_year = ['Unknown','2-5yrs','Above 10yr','5-10yrs','1-2yr','Below 1yr']
+options_services_year = ['Below 1yr','1-2yr','2-5yrs','5-10yrs','Above 10yr','Unknown']
 
-options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas',
+options_acc_area = ['Office areas', 'Residential areas', ' Church areas',
     ' Industrial areas', 'School areas', ' Recreational areas',
     ' Outside rural areas', ' Hospital areas', ' Market areas',
     'Rural village areas', 'Unknown', 'Rural village areasOffice areas',
-    'Recreational areas']
+    'Recreational areas', 'Other']
 
 # features list
 features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
     'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
 
 # Give a title to web app using html syntax
-st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'> App for Predicting Accident Severity  🚗🤕 </h1>", unsafe_allow_html=True)
 
 # define a main() function to take inputs from user in form based approach
 def main():
@@ -85,40 +92,39 @@ def main():
        prediction = model.predict(pred_arr)
         
        if prediction == 0:
-           st.write(f"The severity prediction is fatal injury⚠")
+           st.write(f"The severity prediction is ❗ fatal injury ❗")
        elif prediction == 1:
-           st.write(f"The severity prediction is serious injury")
+           st.write(f"The severity prediction is major injury ⚠")
        else:
-           st.write(f"The severity prediction is slight injury")
+           st.write(f"The severity prediction is minor injury")
         
-       st.write("Developed By: Avi kumar Talaviya")
-       st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
-       [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
-       [Kaggle](https://www.kaggle.com/avikumart) 
+       st.write("Developed By: Lim YuJia")
+       st.markdown("""
+        [Linkedin](https://www.linkedin.com/in/yujia-lim-b85081213/) |
        """)
 
 a,b,c = st.columns([0.2,0.6,0.2])
 with b:
- st.image("banner-picture.jpeg", use_column_width=True)
+ st.image("banner-pic.jpeg", use_column_width=True)
 
 
 # description about the project and code files       
 st.subheader("🧾Description:")
-st.text("""This data set is collected from Addis Ababa Sub-city police departments for master's research work. 
-The data set has been prepared from manual records of road traffic accidents of the year 2017-20. 
-All the sensitive information has been excluded during data encoding and finally it has 32 features and 12316 instances of the accident.
-Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.
+st.text("""
+        This dataset originates from the police departments of Addis Ababa Sub-city and serves as the foundation for master's research endeavors. 
+        The dataset's compilation draws from manual records documenting road traffic accidents spanning 2017 to 2020, encompassing 32 distinct features and 12,316 instances of accidents. 
+        Subsequently, a preprocessing phase is undertaken, followed by the application of diverse machine learning classification algorithms to discern significant accident causes through comprehensive analysis.
 """)
 
 st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
 
-st.subheader("🧭 Problem Statement:")
-st.text("""The target feature is Accident_severity which is a multi-class variable. 
-The task is to classify this variable based on the other 31 features step-by-step by going through each day's task. 
-The metric for evaluation will be f1-score
+st.subheader("Statement of Problem:")
+st.text("""The target feature is `Accident_severity` which is a multi-class data. 
+The task aims to classify this variable based on the other 31 features.
+The metric for evaluation is f1-score.
 """)
 
-st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")          
+st.markdown("This project is insipired by [Stackup](https://app.stackup.dev/campaign_page/python-end-to-end-multiclass-classification-project) and [Avikumar Talaviya](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project). You may find GitHub repository link of this project: [Click Here](https://github.com/Jia02/-Multiclass-Classification-ML-Streamlit)")          
   
 # run the main function        
 if __name__ == '__main__':
