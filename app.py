0de7d2c39fd0912101d35a8c69249fc66f725273
YuJia Lim <yujialim0628@gmail.com>
2023-08-31 15:53:12 +0800

    Update code

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ encoder = joblib.load("ordinal_encoder2.joblib")
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# 1: Major Injury, 2: Minor Injury, 0: Fatal Injury
-
 st.set_page_config(page_title=" Prediction of Vehicle Accident Severity",
         page_icon="🚗", layout="wide")
 
@@ -27,16 +25,16 @@ options_age = ['Under 18', '18-30', '31-50', 'Over 51', 'Unknown']
 # hour of the day: range of 0 to 23
 
 options_types_collision = [
-    'Collision of vehicle with vehicle',
+    'Vehicle with vehicle collision',
     'Collision with roadside objects',
     'Collision with pedestrians',
     'Rollover',
     'Collision with animals',
-    'Unknown',
     'Collision with roadside-parked vehicles',
     'Fall from vehicles',
-    'Other',
-    'With Train']
+    'With Train',
+    'Unknown',
+    'Other',]
 
 options_sex = ['Male','Female','Unknown']
 
@@ -62,46 +60,52 @@ st.markdown("<h1 style='text-align: center;'> App for Predicting Accident Severi
 def main():
     with st.form("road_traffic_severity_form"):
        st.subheader("Please enter the following inputs:")
-        
-       No_vehicles = st.slider("Number of vehicles involved:",1,7, value=0, format="%d")
-       No_casualties = st.slider("Number of casualties:",1,8, value=0, format="%d")
+       Day_week = st.selectbox("Day of the week:", options=options_day)
        Hour = st.slider("Hour of the day:", 0, 23, value=0, format="%d")
        collision = st.selectbox("Type of collision:",options=options_types_collision)
+       No_vehicles = st.slider("Number of vehicles involved:",1,7, value=0, format="%d")
+       No_casualties = st.slider("Number of casualties:",1,8, value=0, format="%d")
+       Accident_area = st.selectbox("Area of accident:", options=options_acc_area)
        Age_band = st.selectbox("Driver age group?:", options=options_age)
        Sex = st.selectbox("Sex of the driver:", options=options_sex)
        Education = st.selectbox("Education of driver:",options=options_education_level)
        service_vehicle = st.selectbox("Service year of vehicle:", options=options_services_year)
-       Day_week = st.selectbox("Day of the week:", options=options_day)
-       Accident_area = st.selectbox("Area of accident:", options=options_acc_area)
-        
+    
        submit = st.form_submit_button("Predict")
 
 # encode using ordinal encoder and predict
     if submit:
-       input_array = np.array([collision,
-                  Age_band,Sex,Education,service_vehicle,
-                  Day_week,Accident_area], ndmin=2)
-        
-       encoded_arr = list(encoder.transform(input_array).ravel())
-        
-       num_arr = [No_vehicles,No_casualties,Hour]
-       pred_arr = np.array(num_arr + encoded_arr).reshape(1,-1)        
+        input_array = np.array([collision,
+                    Age_band,Sex,Education,service_vehicle,
+                    Day_week,Accident_area], ndmin=2)
+            
+        encoded_arr = list(encoder.transform(input_array).ravel())
+            
+        num_arr = [No_vehicles,No_casualties,Hour]
+        pred_arr = np.array(num_arr + encoded_arr).reshape(1,-1) 
       
 # predict the target from all the input features
-       prediction = model.predict(pred_arr)
+# 0: Fatal Injury, 1: Major Injury, 2: Minor Injury
+        prediction = model.predict(pred_arr)
         
-       if prediction == 0:
-           st.write(f"The severity prediction is ❗ fatal injury ❗")
-       elif prediction == 1:
-           st.write(f"The severity prediction is major injury ⚠")
-       else:
-           st.write(f"The severity prediction is minor injury")
-        
-       st.write("Developed By: Lim YuJia")
-       st.markdown("""
-        [LinkedIn](https://www.linkedin.com/in/yujia-lim-b85081213/) | [Github Repository](https://github.com/Jia02/-Multiclass-Classification-ML-Streamlit)
-       """)
-       st.markdown("This project is insipired by [Stackup](https://app.stackup.dev/campaign_page/python-end-to-end-multiclass-classification-project) and [Avikumar Talaviya](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project).")
+        if prediction == 0:
+            text="The severity prediction is ❗ fatal injury ❗"
+            #st.write(f"The severity prediction is ❗ fatal injury ❗")
+        elif prediction == 1:
+            #st.write(f"The severity prediction is major injury ⚠")
+            text="The severity prediction is major injury ⚠"
+        else:
+            #st.write(f"The severity prediction is minor injury")
+            text="The severity prediction is minor injury"
+
+        st.text_area("Level of Severity", text, key='textarea_id')
+        st.markdown('<style>div#textarea_id { text-align:center;font-size:16px;font-weight:bold; }</style>', unsafe_allow_html=True)
+    
+    st.write("Developed By: Lim YuJia")
+    st.markdown("""
+    [LinkedIn](https://www.linkedin.com/in/yujia-lim-b85081213/) | [Github Repository](https://github.com/Jia02/-Multiclass-Classification-ML-Streamlit)
+    """)
+    st.markdown("This project is insipired by [Stackup](https://app.stackup.dev/campaign_page/python-end-to-end-multiclass-classification-project) and [Avikumar Talaviya](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project).")
 
 a,b,c = st.columns([0.2,0.6,0.2])
 with b:
